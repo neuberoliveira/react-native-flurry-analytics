afaf71d082b0651b2b24f7c48f882973d42a1d34
Neuber Oliveira <neuber.oliveira05@gmail.com>
2019-03-06 14:11:00 -0300

    add - methodQueue and requiresMainQueueSetup to ios

--- ios/RNFlurryAnalytics.m
+++ ios/RNFlurryAnalytics.m
@@ -13,6 +13,14 @@
   return self;
 }
 
+-(dispatch_queue_t)methodQueue {
+    return dispatch_get_main_queue();
+}
+
++(BOOL)requiresMainQueueSetup {
+    return YES;
+}
+
 RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(startSession:(NSString *)apiKey)
